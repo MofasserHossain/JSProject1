39c9232344748b93d1e4188038e1d6dc88c90766
MofasserHossain <mofasserhossain1221@gmail.com>
2021-01-22 12:31:22 +0600

    file upload done

---
+++ assignment.js
@@ -0,0 +1,87 @@
+// .convert kilometer to meter
+function kilometerToMeter(kilometer) {
+    var result = kilometer * 1000;
+    // . condition
+    if (kilometer < 0){
+        return "Your Have Enter Wrong Number. Enter Value Above Zero!";
+    }
+    else if (kilometer == 0){
+        return "Enter Value Above Zero!";
+    } 
+    else{
+        return result;
+    }
+}
+var kiloToMeter = kilometerToMeter(3);
+console.log(kiloToMeter);
+
+
+// .find Shopping Cost
+function budgetCalculator(watch, mobile, laptop){
+    // . condition
+    if (watch < 0 || mobile < 0 || laptop < 0) {
+        return "Your Have Enter Wrong Value. Enter Number Above Zero. Happy Shopping..";
+    }
+    else if (watch == 0 || mobile == 0 || laptop == 0) {
+        return "Enter Number Avobe Zero. Minimum Product Quantity is One. Happy Shopping!";
+    }
+    else {
+        var watchPrice = watch * 50,
+            mobilePrice = mobile * 100,
+            laptopPrice = laptop * 500,
+        totalPrice = watchPrice + mobilePrice + laptopPrice;
+        return totalPrice;
+    }
+}
+var totalShoppingCost = budgetCalculator(5, 2, 2);
+console.log(totalShoppingCost);
+
+
+// .Find Hotel cost
+function hotelCost(days){
+    var totalCost = 0;
+    // . condition
+    if (days < 0) {
+        return "You Have Enter Worng Number!! Enter Number Avobe Zero.";
+    }
+    else if(days == 0){
+        return "Enter Your Value Avobe Zero.";
+    }
+    else if (days <= 10) {
+        totalCost = days * 100;
+    }
+    else if (days <= 20) {
+        var firstTenDays = 10 * 100,
+            TenToTwenty = days - 10,
+            remainning = TenToTwenty * 80;
+        totalCost = firstTenDays + remainning;
+    }
+    else{
+        var firstTenDays = 10 * 100,
+            TenToTwentyDays = 10 * 80,
+            TwentyToAbove = days - 20,
+            remainning = TwentyToAbove * 50;
+        totalCost = firstTenDays + TenToTwentyDays + remainning;
+    }
+    return totalCost;
+}
+var totalHouseCost = hotelCost(12);
+console.log(totalHouseCost);
+
+// .Find Mega Frind Name
+function megaFriend(friendsName){
+    var megaFrinedName = friendsName[0];
+    for (var i = 0; i < friendsName.length; i++) {
+        var element = friendsName[i];
+        if(element.length <= 0){
+            return "Unexpected error!! Enter Right Name.";
+        }
+        else if (element.length > megaFrinedName.length) {
+            megaFrinedName = element;
+        }
+    }
+    return megaFrinedName;
+}
+var friendsName = ["Sayed Rabet", "Asif Al Mahbub", "Emon Hossain", "Imam Hossain Emon"];
+var megaName = megaFriend(friendsName);
+console.log(megaName);
